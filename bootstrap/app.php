4d98208f0387d477cbc1afc4337e7312ecda15a3
rachidcooder <cooderherou@gmail.com>
2025-06-14 11:47:09 +0100

    edit for cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Middleware\CheckUserRole;
-use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,19 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
         // Register custom middleware aliases
         $middleware->alias([
             'role' => CheckUserRole::class,
-            'cors' => CorsMiddleware::class,
         ]);
 
-        // Add CORS middleware to API routes
-        $middleware->api(prepend: [
-            CorsMiddleware::class,
-        ]);
-
-        // Add CORS middleware to web routes if needed
-        $middleware->web(prepend: [
-            CorsMiddleware::class,
-        ]);
+       
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        
     })->create();
