b47f4a96afdaf46bc302daf578dde713a6cf7541
rachidcooder <cooderherou@gmail.com>
2025-11-08 18:46:52 +0100

    add comment test

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+//add comments test 
 // require __DIR__.'/api/auth.php';
 require __DIR__.'/api/user.php';
 require __DIR__.'/api/courses.php';
 require __DIR__.'/api/subscribe.php';
 require __DIR__.'/api/quiz.php';
 require __DIR__.'/api/reviews.php';
+
